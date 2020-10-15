90f6b196021fdab452e54cbb88d643800639b7c9
Michael-Tyler <miketylerabc@gmail.com>
2020-10-15 10:10:53 -0500

    first commit

---
+++ walking/animals.py
@@ -0,0 +1,36 @@
+from datetime import date
+
+class Pig:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = True
+
+class Llama:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = True
+
+class Caracal:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = True
+
+class Platypus:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = True
+
+class Camel:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.date_added = date.today()
+        self.walking = True
